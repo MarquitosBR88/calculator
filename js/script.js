155d33a3c05b243625738389686fc348443dc51e
Marcus <marcus.vi.mar@gmail.com>
2024-05-20 12:40:26 -0300

    Add some changes to JS and CSS
    I added some comments and ";" on some lines of code, I also added a different color for the operator buttons.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 //Clicked button
 let clicked = document.querySelectorAll(".number, .decimal");
 //The final number before clicking the operator
-let input = ""
+let input = "";
 //Clicked operator
-let operator = document.querySelectorAll(".add, .subtract, .multiply, .divide, .equals")
-let firstNum
-let secondNum
-let result = 0
-let displayValue = document.querySelector(".display")
+let operator = document.querySelectorAll(".add, .subtract, .multiply, .divide, .equals");
+let firstNum;
+let secondNum;
+let result = 0;
+let displayValue = document.querySelector(".display");
 //Operator of the problem (the operator of the actual operation)
-let problemOperator = ""
+let problemOperator = "";
 let decimalCounter = 0;
 
 operatorFn()
@@ -29,14 +29,14 @@ function operate(operator, a, b) {
 clicked.forEach((button) => {   
     button.addEventListener("click", () => {    
         
-        //These three lines of code represents the number resetting when the user clicks on a number, to avoid appending the clicked number to 0 or to the result
-        displayValue.textContent = ""
+        //These two lines of code represents the number resetting when the user clicks on a number, to avoid appending the clicked number to 0 or to the result
+        displayValue.textContent = "";
         clicked = button.textContent;
         if (clicked === ".") {
             //This if clause and decimalCounter variable was made to prevent multiple "." on the number
             if (decimalCounter === 0) {
                 input += clicked;
-                decimalCounter++
+                decimalCounter++;
             } 
         } else {
             //The input is the final number, while clicked only represents the number that you click on the time, so every number clicked appends to the input
@@ -59,39 +59,42 @@ function operatorFn() {
     operator.forEach((operation) => {   
         operation.addEventListener("click", () => {
             operator = operation.textContent;
-            //The first number will only be modified for being the same as the displayValue on the first operation, after cleaning or after clicking on the equal button.
-            //This is why the first number only turns into a number on this if, because after the if clause, the first number will always be a number and will always be the same as result, for accumulative operations, until the equal button is pressed.
-            //This was made to avoid problems in the accumulative operations (example: 2 + 2 - 3 * 5), and only the second number will be the input after the first operation.
+            /*The first number will only be modified for being the same as the displayValue on the first operation, after cleaning or after clicking on the equal button.
+            This is why the first number only turns into a number on this if, because after the if clause, the first number will always be a number and will always be the same as result, for accumulative operations, until the equal button is pressed.
+            This was made to avoid problems in the accumulative operations (example: 2 + 2 - 3 * 5), and only the second number will be the input after the first operation. */
             if (typeof(firstNum) !== "number") {
-                firstNum = displayValue.textContent
-                firstNum = parseFloat(firstNum)
-                problemOperator = operator
-                result = firstNum
-                result = Math.round((result + Number.EPSILON) * 100) / 100
-                displayValue.textContent = result
+                firstNum = displayValue.textContent;
+                firstNum = parseFloat(firstNum);
+                problemOperator = operator;
+                result = firstNum;
+                result = Math.round((result + Number.EPSILON) * 100) / 100;
+                displayValue.textContent = result;
             } else {
-                secondNum = displayValue.textContent
-                secondNum = parseFloat(secondNum)
-                result = operate(problemOperator, firstNum, secondNum)
-                result = Math.round((result + Number.EPSILON) * 100) / 100
-                displayValue.textContent = result
-                firstNum = result             
+                secondNum = displayValue.textContent;
+                secondNum = parseFloat(secondNum);
+                result = operate(problemOperator, firstNum, secondNum);
+                result = Math.round((result + Number.EPSILON) * 100) / 100;
+                displayValue.textContent = result;
+                firstNum = result;           
             }
 
+            /*This if clause is to avoid the "=" operator to be the operator of the problem (for example, if after we use the "=" we try to divide the result to 2,
+            this block of code will avoid the problemOperator be "=", causing bugs to the code), this will reset the first number as well, after clicking "=", the first number
+            will be a string. */
             if (operator !== "=") {
-                problemOperator = operator
+                problemOperator = operator;
             } else {
-                firstNum = ""
+                firstNum = "";
             }
 
+            //Try dividing for 0
+            if (secondNum === 0 && problemOperator === "/") {
+                alert("R u dumb?");
+                clear();
+            }
+                     
             //Resets the input after each operator button is clicked
-            if (secondNum === 0) {
-                alert("R u dumb?")
-                clear()
-            }
-            
-
-            input = ""
+            input = "";
             decimalCounter = 0;
           
         });
@@ -100,14 +103,14 @@ function operatorFn() {
 
 
 function clear() {
-    displayValue.textContent = 0
-    firstNum = ""
-    operator = document.querySelectorAll(".add, .subtract, .multiply, .divide, .equals")
-    secondNum = ""
-    clicked = document.querySelectorAll(".number")
-    problemOperator = ""
-    result = 0
-    input = ""
+    displayValue.textContent = 0;
+    firstNum = "";
+    operator = document.querySelectorAll(".add, .subtract, .multiply, .divide, .equals");
+    secondNum = "";
+    clicked = document.querySelectorAll(".number");
+    problemOperator = "";
+    result = 0;
+    input = "";
 
 }
 
